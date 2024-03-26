117bbdcbcbcb27c3f24985976a77ec1add6b96aa
hemanth7301 <saihemanthsakhamuri@gmail.com>
2024-03-26 19:31:25 +0530

    CoR

---
+++ ChainofResponsibility/ErrorLogProcessor.java
@@ -0,0 +1,17 @@
+package ChainofResponsibility;
+
+public class ErrorLogProcessor extends LogProcessor{
+
+    ErrorLogProcessor(LogProcessor nextLogProcessor){
+        super(nextLogProcessor);
+    }
+    
+    public void log(int level,String message){
+        if(level==ERROR){
+            System.out.println("ERROR : "+message);
+        }
+        else{
+            super.log(level,message);
+        }
+    }
+}
---
+++ ChainofResponsibility/InfoLogProcessor.java
@@ -0,0 +1,17 @@
+package ChainofResponsibility;
+
+public class InfoLogProcessor extends LogProcessor{
+
+    InfoLogProcessor(LogProcessor nextLogProcessor){
+        super(nextLogProcessor);
+    }
+
+    public void log(int level,String message){
+        if(level==INFO){
+            System.out.println("INFO : "+message);
+        }
+        else{
+            super.log(level,message);
+        }
+    }
+}
