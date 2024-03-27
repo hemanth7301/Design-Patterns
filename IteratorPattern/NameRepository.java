c3131f4a6416ff05759355cb07fead096d72f188
hemanth7301 <saihemanthsakhamuri@gmail.com>
2024-03-27 21:47:31 +0530

    Iterator Pattern

---
+++ IteratorPattern/NameRepository.java
@@ -0,0 +1,21 @@
+package IteratorPattern;
+
+public class NameRepository implements Container{
+    public String names[]={"Robert" , "John" ,"Julie" , "Lora"};
+    public Iterator getIterator(){
+        return new NameIterator();
+    }
+
+    public class NameIterator implements Iterator{
+        int index;
+        public boolean hasNext(){
+            if(index<names.length)return true;
+            return false;
+        }
+        public Object next(){
+            if(this.hasNext()) return names[index++];
+            return null;
+        }
+    }
+
+}
